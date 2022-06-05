a106b7de881e036cf4787f9904fed9d40fae09c2
fiko942 <tobellord@gmail.com>
2022-06-05 18:35:39 +0700

    Route change

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/gh-widget.js', 'Home::index');
+$routes->get('/wiji-fiko-teren/gh-widget.js', 'Home::index');
 
 /*
  * --------------------------------------------------------------------
